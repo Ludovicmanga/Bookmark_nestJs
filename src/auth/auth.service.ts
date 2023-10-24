139afa7233be0494723e75541fd2241e4847675b
Ludovicmanga <ludovic.mangaj@gmail.com>
2023-10-24 10:48:48 +0200

    we set up the jwt system correctly

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,13 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
   async signIn(dto: AuthDto) {
     try {
       const foundUser = await this.prisma.user.findFirst({
@@ -20,7 +24,7 @@ export class AuthService {
       if (!pwMatches) {
         throw new ForbiddenException('Credentials incorrect');
       }
-      return foundUser;
+      return this.signToken(foundUser.id, foundUser.email);
     } catch (error) {
       console.log(error, ' is the error my boy...');
     }
@@ -43,5 +47,26 @@ export class AuthService {
         }
       }
     }
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{
+    access_token: string;
+  }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: process.env.JWT_SECRET,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
